2d47a027507ea84d96b9a6f3f554dc05e007cb5f
kush-s <26009914+Kush-S@users.noreply.github.com>
2021-01-28 17:15:35 -0500

    Added final methods and cleared print statements

--- main.py
+++ main.py
@@ -22,37 +22,37 @@ def main():
     bucket_files = get_bucket_files()
 
     if PRIORITY_PROMPTS_FILE in bucket_files and not check_aws_empty_file(PRIORITY_PROMPTS_FILE):
-        available_prompts = get_available_prompts(PRIORITY_PROMPTS_FILE)
+        available_prompts = get_s3_available_prompts(PRIORITY_PROMPTS_FILE)
         file_used = PRIORITY_PROMPTS_FILE
-        print("priority_prompts has prompts")
     elif GENERAL_PROMPTS_FILE in bucket_files and not check_aws_empty_file(GENERAL_PROMPTS_FILE):
-        available_prompts = get_available_prompts(GENERAL_PROMPTS_FILE)
+        available_prompts = get_s3_available_prompts(GENERAL_PROMPTS_FILE)
         file_used = GENERAL_PROMPTS_FILE
-        print("general_prompts has prompts")
     else:
-        create_aws_s3_file(GENERAL_PROMPTS_FILE)
-        available_prompts = get_available_prompts(GENERAL_PROMPTS_FILE)
+        local_prompts = get_local_prompts()
+        store_available_prompts(GENERAL_PROMPTS_FILE, local_prompts)
+        available_prompts = get_s3_available_prompts(GENERAL_PROMPTS_FILE)
         file_used = GENERAL_PROMPTS_FILE
 
     reddit = bot_login()
     recent_prompts = get_recent_prompts(reddit, NUMBER_OF_DAYS)
 
     new_prompt = generate_new_promp(available_prompts)
-    if new_prompt in recent_prompts:
-        collisions = 0
-        while collisions <= MAX_PROMPT_CHECKS:
-            new_prompt = generate_new_promp(available_prompts)
-            if new_prompt not in recent_prompts:
-                break
-            else:
-                collisions += collisions
+
+    if file_used != PRIORITY_PROMPTS_FILE:
+        if new_prompt in recent_prompts:
+            collisions = 0
+            while collisions <= MAX_PROMPT_CHECKS:
+                new_prompt = generate_new_promp(available_prompts)
+                if new_prompt not in recent_prompts:
+                    break
+                else:
+                    collisions += collisions
 
     date_today = get_date_today()
     title = config.reddit_submission_prefix + ' for ' + date_today + '--' + new_prompt
-    print(title)
-    # reddit.subreddit(config.reddit_subreddit).submit(title, selftext='', send_replies=False)
-    # available_prompts.remove(new_prompt)
-    # store_available_prompts(file_used, available_prompts)
+    reddit.subreddit(config.reddit_subreddit).submit(title, selftext='', send_replies=False)
+    available_prompts.remove(new_prompt)
+    store_available_prompts(file_used, available_prompts)
     return
 
 # Helpers methods------------
@@ -73,7 +73,7 @@ def check_aws_empty_file(file_name):
         return True
     else:
         return False
-def get_available_prompts(file_name):
+def get_s3_available_prompts(file_name):
     aws_s3 = get_aws_s3_session()
     prompts = []
 
@@ -83,8 +83,14 @@ def get_available_prompts(file_name):
 
     for line in file_data:
         prompts.append(line)
-
     return prompts
+def get_local_prompts():
+    prompt_list = []
+    prompt_file = open('prompts.txt', 'r')
+
+    for prompt in prompt_file:
+        prompt_list.append(prompt.strip())
+    return prompt_list
 def get_aws_s3_session():
     aws_s3_session = boto3.Session(
         region_name = config.aws_s3_bucket_region,
@@ -100,11 +106,6 @@ def get_bucket_files():
     for file in s3_object_summary:
         files_list.append(file.key)
     return files_list
-def create_aws_s3_file(file_name):
-    aws_s3 = get_aws_s3_session()
-
-    aws_s3.Bucket(config.aws_s3_bucket_name).put_object(Key=file_name)
-    return
 def bot_login():
     reddit = praw.Reddit(
             username = config.reddit_username,
@@ -114,15 +115,22 @@ def bot_login():
             user_agent = config.reddit_user_agent)
     return reddit
 def generate_new_promp(prompt_list):
-    random_num = random.randint(1, len(prompt_list) - 1)
+    random_num = random.randint(0, len(prompt_list) - 1)
     return prompt_list[random_num]
 def get_date_today():
     date_today = datetime.today().strftime("%m/%d/%y")
     return date_today
 def store_available_prompts(file_name, prompts):
+    prompt_string = ''
+
+    while len(prompts) > 0:
+        prompt_string += prompts.pop(0) + '\n'
+
+    prompt_string = prompt_string.strip()
+
     aws_s3 = get_aws_s3_session()
     aws_object = aws_s3.Object(config.aws_s3_bucket_name, file_name)
-    aws_object.put(Body=str(prompts))
+    aws_object.put(Body=prompt_string)
     return
 if __name__ == "__main__":
     main()
